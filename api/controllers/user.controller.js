f88843950f2b852efc5171f48c391d880375c52f
laureschaeffer <laure.schaeffer@hotmail.com>
2024-07-17 16:31:30 +0200

    show users to the admin dashboard

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -84,8 +84,8 @@ export const getUsers = async (req, res, next) => {
 
     try {
         const startIndex = parseInt(req.query.startIndex) || 0;
-        const limit = parseInt(req.quert.limit) || 9;
-        const sortDirection = req.quert.sort === 'asc' ? 1 : -1 ;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1 ;
 
         const users = await User.find()
             .sort({ createdAt: sortDirection })
