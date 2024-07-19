aad13ace0d1f54fa211e8e10f6d82880ec12fb8f
laureschaeffer <laure.schaeffer@hotmail.com>
2024-07-19 10:18:02 +0200

    add like functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -37,3 +37,32 @@ export const getPostComments = async (req, res, next) => {
     }
 
 }
+
+export const likeComment = async (req, res, next) => {
+
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if(!comment){
+            return next(errorHandler(404, 'Comment not found'));
+        }
+
+        //search in the likes array if there's the user id
+        const userIndex = comment.likes.indexOf(req.user.id);
+
+        if(userIndex === -1){
+            comment.numberOfLikes +=1;
+            comment.likes.push(req.user.id)
+        } else {
+            //if user id found, remove it (1)
+            comment.likes.splice(userIndex, 1);
+            comment.numberOfLikes -=1;
+        }
+
+        await comment.save();
+        res.status(200).json(comment);
+
+    } catch (error) {
+        next(error);
+    }
+
+}
