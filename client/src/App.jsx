c4769ea8d3e2acb06059f501648b217ef7a75fa7
laureschaeffer <laure.schaeffer@hotmail.com>
2024-07-22 16:17:42 +0200

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from './pages/CreatePost.jsx'
 import UpdatePost from './pages/UpdatePost.jsx'
 import PostPage from './pages/PostPage.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
+import Search from './pages/Search'
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
         {/* make dashboard private by creating a private wrapper component */}
         <Route element={<PrivateRoute />}> 
           <Route path='/dashboard' element={<Dashboard />} />
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,90 @@
+import { TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function Search() {
+
+    const [sidebarData, setSidebarData] = useState({
+        searchTerm: '',
+        sort: 'desc',
+        category: 'uncategorized'
+    });
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+    const location = useLocation();
+
+    
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        const sortFromUrl = urlParams.get('sort');
+        const categoryFromUrl = urlParams.get('category');
+        
+        if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+          setSidebarData({
+            ...sidebarData,
+            searchTerm: searchTermFromUrl,
+            sort: sortFromUrl,
+            category: categoryFromUrl,
+          });
+        }
+    
+        const fetchPosts = async () => {
+          setLoading(true);
+          const searchQuery = urlParams.toString();
+          const res = await fetch(`/api/post/getposts?${searchQuery}`);
+
+          if (!res.ok) {
+            setLoading(false);
+            return;
+          }
+
+          if (res.ok) {
+            const data = await res.json();
+            setPosts(data.posts);
+            setLoading(false);
+            if (data.posts.length === 9) {
+              setShowMore(true);
+            } else {
+              setShowMore(false);
+            }
+          }
+        };
+        fetchPosts();
+
+
+      }, [location.search]);
+
+
+    const handleChange = (e) => {
+    if (e.target.id === 'searchTerm') {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+    if (e.target.id === 'sort') {
+      const order = e.target.value || 'desc';
+      setSidebarData({ ...sidebarData, sort: order });
+    }
+    if (e.target.id === 'category') {
+      const category = e.target.value || 'uncategorized';
+      setSidebarData({ ...sidebarData, category });
+    }
+  };
+    
+
+  return (
+    <div>
+      <div className="">
+        <form>
+            <div className="">
+                <label>Search term</label>
+                <TextInput type='text' placeholder='Search...' id='searchTerm' value={sidebarData.searchTerm}
+                onChange={handleChange}
+                />
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+}
